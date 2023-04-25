b8dfb8c66a288482e22d880a2aa69c9e1027acad
yang <64903587+HongYang01@users.noreply.github.com>
2023-04-25 10:14:35 +0800

    New Update

--- index.php
+++ index.php
@@ -5,33 +5,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/asset/image/logo.svg" type="image/x-icon">
     <link rel="stylesheet" href="/css/main.css">
-    <link rel="stylesheet" href="/css/colour-palette.css">
-    <link rel="stylesheet" href="/css/nav-bar.css">
+    <script src="/js/effect.js"></script>
     <title>Semicolonix</title>
+
 </head>
 
 <body>
 
-    <!-- may edit to see changes -->
-    <div class="flex-row">
+    <div id="loader">
+        <iframe src="/asset/loading.svg" title="logo"></iframe>
+    </div>
+
+    <?php
+    $ds = DIRECTORY_SEPARATOR;
+    require_once(__DIR__ . "{$ds}php{$ds}nav.php");
+    ?>
+
+    <!-- <div class="flex-row">
         <p>Color Palette</p>
         <div class="color-palette flex-row">
             <span class="gray" title="Gray"></span>
-            <span class="black" title="White"></span>
+            <span class="black" title="Black"></span>
             <span class="orange" title="Orange"></span>
             <span class="paleOrange" title="Pale Orange"></span>
             <span class="peach" title="Peach"></span>
         </div>
-    </div>
+    </div> -->
 
-    <p class="text-title black">Semicolonix Web Host Service</p>
-
-    <?php
-    $ds = DIRECTORY_SEPARATOR;
-    $base_dir = "C:{$ds}Users{$ds}Administrator{$ds}Desktop{$ds}tmf2234-web-based-assignment{$ds}";
-    include_once("{$base_dir}pages{$ds}nav.php");
-    ?>
+    <p class="black text-title font-second font-w-300">YOUR WEBSITE DESERVES A HOME</p>
 
 </body>
 
--- pages/nav.php
+++
@@ -1,5 +0,0 @@
-<div class="nav-bar gray flex-row">
-    <button class="black">Home</button>
-    <button class="black">Page 1</button>
-    <button class="black">Page 2</button>
-</div>
